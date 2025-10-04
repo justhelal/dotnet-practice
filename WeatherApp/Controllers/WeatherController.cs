d8de55a8487a0719620a555f6b953736e5313cfe
helal <hazemhelal.se@gmail.com>
2025-10-04 20:56:06 +0300

    Initial commit

---
+++ WeatherApp/Controllers/WeatherController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WeatherApp.Models;
+
+namespace WeatherApp.Controllers;
+
+public class WeatherController: Controller
+{
+
+    private List<Weather> _weathers = new List<Weather>()
+    {
+        new Weather(){ CityUniqueCode = "LDN", CityName = "London", DateAndTime = "2030-01-01 8:00",  TemperatureFahrenheit = 33},
+        new Weather(){ CityUniqueCode = "NYC", CityName = "London", DateAndTime = "2030-01-01 3:00",  TemperatureFahrenheit = 60 },
+        new Weather(){ CityUniqueCode = "PAR", CityName = "Paris", DateAndTime = "2030-01-01 9:00",  TemperatureFahrenheit = 82}
+    };
+    
+    [Route("/weather")]
+    [Route("/")]
+    public IActionResult All()
+    {
+        return View(_weathers);
+    }
+
+    [Route("/weather/{id}")]
+    public IActionResult One(string id)
+    {
+        var weather = _weathers.FirstOrDefault(i => i.CityUniqueCode == id);
+        return View(weather);
+    }
+}
---
+++ WeatherApp/Models/Weather.cs
@@ -0,0 +1,9 @@
+﻿namespace WeatherApp.Models;
+
+public class Weather
+{
+    public string CityUniqueCode { get; set; } = string.Empty;
+    public string CityName { get; set; } = string.Empty;
+    public string DateAndTime { get; set; } = string.Empty;
+    public int TemperatureFahrenheit { get; set; }
+}
